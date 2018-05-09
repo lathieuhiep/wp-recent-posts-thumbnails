c2537f6ab919ed4fc7f2ba38b7199ae6ecf29a94
lathieuhiep <khacdiepkma90@gmail.com>
2018-05-09 17:00:47 +0700

    create file wp-recent-posts-thumbs

---
+++ wp-recent-posts-thumbs.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Recent Posts Thumbs Widget
+Plugin URI: https://www.facebook.com/lathieuhiep
+Description: Recent Posts Thumbs Widget list, slider.
+Version: 1.0.0
+Author: La Thiếu Hiệp
+Author URI: https://www.facebook.com/lathieuhiep
+License: GPLv2 or later
+Text Domain: wp-recent-posts-thumbs
+*/
