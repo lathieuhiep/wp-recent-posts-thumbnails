5fa495b3980634e684e2289eb58aa2c37ad91372
lathieuhiep <khacdiepkma90@gmail.com>
2018-05-11 16:27:07 +0700

    include function, hook

--- includes/wp-recent-posts-includes.php
+++ includes/wp-recent-posts-includes.php
@@ -4,5 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-// Include widget classes.
+// Include hook
+include_once 'wp-recent-hook.php';
+
+// Include function
+include_once 'wp-recent-function.php';
+
+// Include widget classes
 include_once 'widgets/class-widget-recent-posts.php';
