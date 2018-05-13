4c69227c44c7b1493c423668c306ccc41caaadd2
lathieuhiep <khacdiepkma90@gmail.com>
2018-05-13 23:17:38 +0700

    code function recent post

--- includes/wp-recent-function.php
+++ includes/wp-recent-function.php
@@ -2,17 +2,16 @@
 /* Output recent post */
 if ( ! function_exists( 'wp_recent_posts_thumbs_item' ) ) :
 
-    function wp_recent_posts_thumbs_item( $wp_recent_posts_thumbs_item_class = '', $wp_recent_posts_thumbs_hide_date = false ) {
+    function wp_recent_posts_thumbs_item( $wp_recent_posts_thumbs_hide_date = false, $wp_recent_posts_thumbs_type_post = 'list', $wp_recent_posts_thumbs_query ) {
+
 ?>
 
-        <div class="wp_recent_posts_thumbs_item <?php echo esc_attr( $wp_recent_posts_thumbs_item_class ); ?>">
+        <div class="wp_recent_posts_thumbs_item">
             <div class="wp_recent_posts_thumbs_item--image">
                 <?php
-
                 if ( has_post_thumbnail() ) :
-                    the_post_thumbnail( 'thumbnail' );
+                    the_post_thumbnail( 'medium' );
                 else:
-
                 ?>
 
                     <img src="<?php echo esc_url( wp_recent_posts_thumbs_path . 'assets/images/no-image.png' ) ?>" alt="<?php the_title(); ?>">
@@ -32,6 +31,25 @@ if ( ! function_exists( 'wp_recent_posts_thumbs_item' ) ) :
                     <span class="wp_recent_posts_thumbs_item--meta">
                         <?php echo get_the_date(); ?>
                     </span>
+
+                <?php endif; ?>
+
+                <?php if ( $wp_recent_posts_thumbs_type_post == 'fist_large_post' && $wp_recent_posts_thumbs_query->current_post == 0 ) : ?>
+
+                    <div class="wp_recent_posts_thumbs_item--describe">
+                        <?php
+                        if ( has_excerpt() ) :
+                            the_excerpt();
+                        else:
+                        ?>
+
+                            <p>
+                                <?php echo wp_trim_words( get_the_content(), 20, '...' ); ?>
+                            </p>
+
+                        <?php endif; ?>
+
+                    </div>
 
                 <?php endif; ?>
             </div>
