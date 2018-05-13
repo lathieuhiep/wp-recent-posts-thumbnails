de13031e6ff43d2f9b74b9435315c903995c3455
lathieuhiep <khacdiepkma90@gmail.com>
2018-05-13 23:18:11 +0700

    remove code thumbs item

--- includes/widgets/class-widget-recent-posts.php
+++ includes/widgets/class-widget-recent-posts.php
@@ -87,58 +87,8 @@ class Wp_Recent_Posts_Thumbs_Widget extends WP_Widget {
                 while ( $wp_recent_posts_thumbs_query->have_posts() ) :
                     $wp_recent_posts_thumbs_query->the_post();
 
-                ?>
+                    do_action( 'wp_recent_post_thumbs_item_content', $wp_recent_posts_thumbs_hide_date, $wp_recent_posts_thumbs_type_post, $wp_recent_posts_thumbs_query );
 
-                    <div class="wp_recent_posts_thumbs_item">
-                        <div class="wp_recent_posts_thumbs_item--image">
-                            <?php
-                            if ( has_post_thumbnail() ) :
-                                the_post_thumbnail( 'medium' );
-                            else:
-                            ?>
-
-                                <img src="<?php echo esc_url( wp_recent_posts_thumbs_path . 'assets/images/no-image.png' ) ?>" alt="<?php the_title(); ?>">
-
-                            <?php endif; ?>
-                        </div>
-
-                        <div class="wp_recent_posts_thumbs_item--content">
-                            <h3 class="wp_recent_posts_thumbs_item--title">
-                                <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
-                                    <?php the_title(); ?>
-                                </a>
-                            </h3>
-
-                            <?php if ( $wp_recent_posts_thumbs_hide_date == false ) : ?>
-
-                                <span class="wp_recent_posts_thumbs_item--meta">
-                                    <?php echo get_the_date(); ?>
-                                </span>
-
-                            <?php endif; ?>
-
-                            <?php if ( $wp_recent_posts_thumbs_type_post == 'fist_large_post' && $wp_recent_posts_thumbs_query->current_post == 0 ) : ?>
-
-                                <div class="wp_recent_posts_thumbs_item--describe">
-                                    <?php
-                                    if ( has_excerpt() ) :
-                                        the_excerpt();
-                                    else:
-                                    ?>
-
-                                        <p>
-                                            <?php echo wp_trim_words( get_the_content(), 20, '...' ); ?>
-                                        </p>
-
-                                    <?php endif; ?>
-
-                                </div>
-
-                            <?php endif; ?>
-                        </div>
-                    </div>
-
-                <?php
                 endwhile;
                 wp_reset_postdata();
                 ?>
